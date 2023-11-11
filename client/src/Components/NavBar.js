8eca76e0cf9635b04fddb94c1e2c147306a67223
Rakesh <rakeshcse123@gmail.com>
2023-11-11 12:56:44 +0530

    Add NavBar and Footer

---
+++ client/src/Components/NavBar.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "../Components/NavBar.css"
+import Logo from "../Images/logo.png"
+import { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+
+const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <div className="navbar-logo">
+          <NavLink to="/home"> <img src={Logo} /> </NavLink>
+        </div>
+        <div className={`navbar-links ${isOpen ? "active" : ""}`}>
+          <Link to='/home'>Home</Link>
+          <Link to='/about'>About</Link>
+          <Link to='/product'>Products</Link>
+          <Link to='/teams'>Teams</Link>
+          <Link to='/investors'>Investors</Link>
+          <Link to='/contactus'>Contact Us</Link>
+        </div>
+        <div className="navbar-toggle" onClick={toggleNavbar}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
---
+++ client/src/Pages/AboutUs.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const AboutUs = () => {
+  return (
+    <div>Hello from About</div>
+  )
+}
+
+export default AboutUs
