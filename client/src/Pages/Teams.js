004f3f26dbd17f021ed94ebbf0581e437dcb810a
Rakesh <rakeshcse123@gmail.com>
2023-11-13 09:28:36 +0530

    Update Teams.js and AboutUs.js file

--- client/src/Pages/Teams.js
+++ client/src/Pages/Teams.js
@@ -1,27 +1,36 @@
 import React from 'react'
 import TeamCard from '../Components/TeamCard'
-import Dev from "../Images/testimage.jpeg"
+import Team1 from "../Images/Teams/Team1.jpeg"
+import Team2 from "../Images/Teams/Team2.jpeg"
+import Team3 from "../Images/Teams/Team3.jpeg"
+import Team4 from "../Images/Teams/Team4.jpeg"
 
 const Teams = () => {
 
   const teamMembers = [
     {
-      name: 'John Doe',
-      role: 'Developer',
-      image: `${Dev}`,
-      bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      name: 'Darshan Reddy G',
+      role: 'Managing Director',
+      image: `${Team1}`,
+      bio: 'Darshan Reddy G, a proficient MBA graduate, guides the company to triumph by seamlessly blending expertise in marketing, finance, supply chain, and innovation. His visionary leadership catalyzes strategic growth initiatives and ensures organizational excellence.',
     },
     {
-      name: 'John Doe',
-      role: 'Developer',
-      image: `${Dev}`,
-      bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      name: 'Dr Muthukumar Chockalingam',
+      role: 'Director, R&D',
+      image: `${Team2}`,
+      bio: 'Dr. Muthukumar Chockalingam, R&D Director at a startup, spearheads cutting-edge research in healthcare, diagnostics, learning, mental health, and sensor technology, pioneering innovations at the intersection of technology and well-being.',
     },
     {
-      name: 'John Doe',
-      role: 'Developer',
-      image: `${Dev}`,
-      bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      name: 'Krishna Raju R',
+      role: 'Chief Technological Officer',
+      image: `${Team3}`,
+      bio: 'Krishna Raju R, CTO, excels in product design, hardware development, and robotics. A mechatronics engineer, proficient in 3D printing, CAD, and effective project management, contributing to innovative solutions at the intersection of technology and engineering.',
+    },
+    {
+      name: 'Rakesh Kumar',
+      role: 'Software Engineer',
+      image: `${Team4}`,
+      bio: 'Rakesh Kumar, SDE, a computer science engineer and professional web developer, combines technical expertise with creativity to craft dynamic and efficient websites. His skills contribute to seamless digital experiences, reflecting a passion for innovation.',
     },
     // Add more team members as needed
   ];
