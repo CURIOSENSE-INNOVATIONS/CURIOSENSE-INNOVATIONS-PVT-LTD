57497eed11474c769086bd4b8b1ae28791b63cad
Rakesh <rakeshcse123@gmail.com>
2023-12-15 13:31:35 +0530

    Bug Test in server

--- server/src/app.js
+++ server/src/app.js
@@ -4,13 +4,15 @@ import cookieParser from "cookie-parser"
 
 const app = express()
 
-app.use(cors({
+const corsOptions = {
     origin: process.env.CORS_ORIGIN,
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     headers: "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers",
+    allowedHeaders: '*',
     credentials: true,
-    
-}))
+}
+
+app.use(cors(corsOptions))
 
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
